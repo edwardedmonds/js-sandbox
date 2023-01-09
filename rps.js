5de458118cb10240643a8d96f1d1922787d37200
Edward Edmonds <edward.edmonds@gmail.com>
2023-01-09 07:35:43 -0500

    Fixed a bug where ties were wins losses ties

--- rps.js
+++ rps.js
@@ -4,6 +4,7 @@ import boxen from 'boxen';
 import Table from 'cli-table3';
 import fs from 'fs';
 import brain from 'brain.js';
+import ora from 'ora';
 
 const scoreCard = {
   numberOfRoundsToPlay: 0,
@@ -33,9 +34,14 @@ function saveScoreCard(scoreCard) {
 let net;
 
 // This function trains the neural network on the player's past selections
-function trainNeuralNetwork() {
+async function trainNeuralNetwork() {
+  // Create a new spinner
+  const spinner = ora('Thinking...').start();
+
   // Load the data from the scoreCard.json file
-  const data = JSON.parse(fs.readFileSync('scoreCard.json'));
+  const data = await fs.promises
+    .readFile('scoreCard.json', 'utf8')
+    .then((data) => JSON.parse(data));
 
   // Create an array of all the player's past selections
   const playerSelections = data.history.map((round) => round.playerSelection);
@@ -45,18 +51,21 @@ function trainNeuralNetwork() {
 
   // Train the neural network on the player's past selections
   net.train(playerSelections, {
-    iterations: 20,
+    iterations: 15,
   });
+
+  // Stop the spinner
+  spinner.stop();
 }
 
 // This function uses the trained neural network to predict the player's next selection
-function computerChoice() {
+function computerChoice(playerSelections) {
   // Use the neural network to predict the player's next selection
   const prediction = net.run(playerSelections);
 
   // Return a random choice if the prediction is not rock, paper, or scissors
   if (!['rock', 'paper', 'scissors'].includes(prediction)) {
-    return computerChoice();
+    return computerChoice(playerSelections);
   }
 
   // Return the predicted choice
@@ -91,17 +100,33 @@ async function playRound(round, playerSelection, computerSelection) {
   switch (result) {
     case 'tie':
       scoreCard.ties++;
-      return chalk.blue('tie!'.toUpperCase());
+      return 'Tie';
+      break;
     case 'win':
       scoreCard.playerWins++;
-      return chalk.green(`${scoreCard.playerName} wins!`.toUpperCase());
+      return `${scoreCard.playerName} wins!`;
+      break;
     case 'loss':
       scoreCard.computerWins++;
-      return chalk.magenta('computer wins!'.toUpperCase());
+      return 'Computer wins!';
+      break;
+    default:
+      return 'Tie';
   }
+
+  // Save the updated score card after each round
+  saveScoreCard(scoreCard);
 }
 
 function getResult(playerSelection, computerSelection) {
+  if (!['rock', 'paper', 'scissors'].includes(playerSelection)) {
+    throw new Error(`Invalid player selection: ${playerSelection}`);
+  }
+
+  if (!['rock', 'paper', 'scissors'].includes(computerSelection)) {
+    throw new Error(`Invalid computer selection: ${computerSelection}`);
+  }
+
   const results = new Map();
   results.set('rock', {
     rock: 'tie',
@@ -109,14 +134,14 @@ function getResult(playerSelection, computerSelection) {
     paper: 'loss',
   });
   results.set('paper', {
-    rock: 'loss',
-    scissors: 'tie',
-    paper: 'win',
-  });
-  results.set('scissors', {
     rock: 'win',
     scissors: 'loss',
     paper: 'tie',
+  });
+  results.set('scissors', {
+    rock: 'loss',
+    scissors: 'tie',
+    paper: 'win',
   });
 
   return results.get(playerSelection)[computerSelection];
@@ -196,6 +221,7 @@ async function playGame() {
   await askHowManyRoundsToPlay();
 
   for (let i = 0; i < scoreCard.numberOfRoundsToPlay; i++) {
+    await trainNeuralNetwork();
     await playRound(i + 1, await playerChoice(), computerChoice()).then(
       (whoWon) => {
         console.log(whoWon);
